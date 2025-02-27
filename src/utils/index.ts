c815452f92a639d61ea6850dfa7392b92a2a8b7b
nyzs <oknakoca@gmail.com>
2025-02-27 11:38:28 +0100

    feat: updated user agent to googlebot

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,12 @@
 import ogs from "open-graph-scraper-lite";
 
 export const getOpenGraphData = async (url: string) => {
-  const html = await fetch(url);
+  const html = await fetch(url, {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+    },
+  });
   const htmlText = await html.text();
 
   const options = {
